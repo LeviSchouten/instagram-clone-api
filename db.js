bea81e7cbeb97f652ac89d393ff2058f88ffed91
LeviSchouten <levischoutenschool@gmail.com>
2020-05-12 12:12:03 +0200

    changed databse info

--- db.js
+++ db.js
@@ -1,9 +1,7 @@
 module.exports = require('knex')({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'levi',
-    password : '',
-    database : 'instagram-clone'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
