0f962166bf4df6cee4b971ff5307338cd1e42487
LeviSchouten <levischoutenschool@gmail.com>
2020-05-14 19:39:34 +0200

    trying heroku cookies fix

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://instagram-clone-levi-frontend.herokuapp.com/"
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.set("trust proxy", 1);
